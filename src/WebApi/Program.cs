7c7602679782a4b9b8a3205a8a0d2e47f2af4e74
cpozos <carlosdevelopment@hotmail.com>
2022-01-14 23:09:58 -0600

    Restructure project

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,9 +1,12 @@
+using Yoli.Core.App.Repositories;
+using Yoli.Core.Infraestructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
