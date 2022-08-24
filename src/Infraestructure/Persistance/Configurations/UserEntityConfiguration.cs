b610c9a4215dfb1e84d6bf8c4db807254f36bc41
cpozos <carlosdevelopment@hotmail.com>
2022-08-23 23:25:41 -0500

    Add persistance initialization

---
+++ src/Infraestructure/Persistance/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Persistance.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infraestructure.Persistance;
+
+internal class UserEntityConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+        builder.HasKey(u => u.Id);
+    }
+}
---
+++ src/Infraestructure/Persistance/YoliDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Persistance.Entities;
+
+namespace Infraestructure.Persistance;
+
+public class YoliDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public YoliDbContext(DbContextOptions<YoliDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserEntityConfiguration());
+        modelBuilder.Entity<User>().HasData(new User
+        {
+            Id = 1,
+            Email = "",
+            Name = "",
+            Password = "",
+            Salt = ""
+        });
+    }
+}
