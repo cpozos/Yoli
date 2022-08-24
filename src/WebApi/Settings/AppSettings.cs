5b6576388a48e3162729658d2328777c2a57703f
cpozos <carlosdevelopment@hotmail.com>
2022-08-23 23:25:59 -0500

    Add test classes

--- src/WebApi/Settings/AppSettings.cs
+++ src/WebApi/Settings/AppSettings.cs
@@ -3,4 +3,24 @@
     public class AppSettings
     {
     }
+
+    // https://stackoverflow.com/questions/1212742/xml-serialize-generic-list-of-serializable-objects
+    [System.Xml.Serialization.XmlInclude(typeof(Test2))]
+    public class Test
+    {
+        [System.Xml.Serialization.XmlArray("MyList")]
+        [System.Xml.Serialization.XmlArrayItem("TestItem")]
+        public List<Test2> List { get; set; } = new List<Test2>();
+    }
+
+    public class Test2
+    {
+        public string Name { get; set; } = "2";
+        public Test3 Test3 { get; set; }
+    }
+
+    public class Test3
+    {
+        public string Name { get; set; }
+    }
 }
